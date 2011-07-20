c4c6e8283e7027a9b80772b4cdc1590445e8e941
Nate Vack <njvack@freshforever.net>
2011-07-20 15:44:15 -0500

    Show task days on participant admin form

--- sampler/admin.py
+++ sampler/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from . import models
 
+class TaskDayInlineAdmin(admin.TabularInline):
+    model = models.TaskDay
+    
+    fields = ('task_day', 'start_time', 'end_time')
+    
+    extra = 0
+
+class ParticipantAdmin(admin.ModelAdmin):
+    inlines = [
+        TaskDayInlineAdmin
+    ]
+
 admin.site.register(models.Experiment)
-admin.site.register(models.Participant)
+admin.site.register(models.Participant, ParticipantAdmin)
 admin.site.register(models.TaskDay)
