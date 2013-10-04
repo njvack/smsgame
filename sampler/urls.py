8eba6db230438cde79dd42738922ba2f4ad750a7
Nate Vack <njvack@wisc.edu>
2013-10-04 16:49:32 -0500

    Allow creating targets.

--- sampler/urls.py
+++ sampler/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns(''
-    ,url(r'^add_participant$', 'sampler.views.add_participant')
+    ,url(r'^experiments/(?P<slug>[A-Za-z0-9_]+)/add_target$',
+        'sampler.views.add_target')
     ,url(r'^tropo$', 'sampler.views.tropo')
     ,url(r'^experiments/(?P<slug>[A-Za-z0-9_]+)/experiencesamples.csv$',
         'sampler.views.experiencesamples_csv')
